5a3bf617007e68ee8fe0b1cf7eb28daf35ca30da
vic <pyatakvictor2@gmail.com>
2022-11-24 18:29:35 +0200

    master: initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+const {} = require('aws-sdk');
+const { configs } = require('./configs');
+
+(async () => {})();
---
+++ configs.js
@@ -0,0 +1,3 @@
+exports.configs = {
+  region: process.env.REGION,
+};
